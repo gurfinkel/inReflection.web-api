385862690344bbdb30673cc36a40f8f143213ee2
Dmitry Blinov <dmitry.blinov@booking.com>
2019-10-11 17:47:24 +0200

    fix problem with mongodb connection

--- src/db.js
+++ src/db.js
@@ -17,7 +17,8 @@ const options = {
 
 const url = `mongodb://${ MONGO_USERNAME }:${ MONGO_PASSWORD }@${ MONGO_HOSTNAME }:${ MONGO_PORT }/${ MONGO_DB }?authSource=admin`;
 
-mongoose.connect(url, options).then(function() {
+mongoose.connect(url, options)
+    .then(function() {
         console.log('MongoDB is connected');
     })
     .catch(function(err) {
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const db = require('./db');
 const bodyParser = require('body-parser');
 
 const routes = require('./api/routes');
