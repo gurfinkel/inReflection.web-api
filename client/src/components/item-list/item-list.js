540e9ceed485d5e9e3f5646f9195c7f5d9dfccc4
Dmitry Blinov <dmitry.blinov@booking.com>
2019-12-04 17:24:45 +0100

    add dockerfile to client app

--- client/src/components/item-list/item-list.js
+++ client/src/components/item-list/item-list.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
+import LooksApiService from "../../services/looks-api-service";
+
 import './item-list.css';
 
 export default class ItemList extends Component {
-    
+
+    looksApiService = new LooksApiService();
+
+    state = {
+        items: null,
+    };
+
+    componentDidMount() {
+        this.looksApiService
+            .getAllGarments()
+            .then((items) => {
+                this.setState({
+                    items,
+                });
+            });
+    }
+
     render() {
+        const { items } = this.state;
+
         return (
             <ul className="item-list list-group">
                 <li className="list-group-item">
--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const db = require('./db');
 
 const routes = require('./api/routes');
 
 const app = express();
 
-const port = process.env.SERVER_PORT || 3000;
+const port = process.env.SERVER_APP_PORT || 3000;
+
+app.use(cors());
 
 // Parse URL-encoded bodies (as sent by HTML forms)
 app.use(express.urlencoded());
@@ -13,7 +16,7 @@ app.use(express.urlencoded());
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
-app.use('/', routes);
+app.use('/api', routes);
 
 app.listen(port, function () {
     console.log(`API Host: localhost:${port}`);
